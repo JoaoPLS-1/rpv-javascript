26cb344dc35a9e446c920f6e0c221014a89b02b6
JoaoPLS-1 <barrancoij@gmail.com>
2025-12-10 20:50:28 -0300

    Variaveis

---
+++ introducao-javascript/variaveis/nomeclaturas.js
@@ -0,0 +1,11 @@
+// Algumas regras a serem seguidas ao nomear variáveis. 
+// O nome de uma variável não deve conter espaços;
+// O nome de uma variável não deve conter operadores;
+// O nome de uma variável não deve ser só um número;
+// Embora seja permitido em JavaScript, o uso de acentos não é permitido em muitas linguagens de programação, por isso recomendamos evitá - los;
+// Algumas linguagens de programação diferenciam letras maiúsculas de letras minúsculas, logo: Nome pode ser diferente de nome;
+// Variáveis cujo nome seja um substantivo composto, como "data de nascimento", podem ser nomeadas com o padrão camelCase: dataDeNascimento.Este padrão define que todas as palavras após a primeira devem começar com letra maiúscula.Outra alternativa é utilizar um traço inferior(_), ou underline, entre cada palavra: data_de_nascimento.
+
+// diferença básica entre const e let ( não é necessário, atualmente, mais utilizar var )
+// const -> não permite reatribuição de valor
+// Ex: 
